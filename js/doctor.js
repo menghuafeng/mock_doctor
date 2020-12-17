4f60653086a82ef3aa9b9a3c2a70a084828839e9
v_menghuafeng <mhf_159357>
2020-12-17 19:12:27 +0800

    20201202-基础页面80%

---
+++ js/doctor.js
@@ -0,0 +1,5 @@
+let proportion = width / 1920;
+$light.style.height = Math.round(618 * proportion) + 'px';
+$light.style.marginLeft = Math.round(65 * proportion) + 'px';
+$light.style.marginTop = Math.round(-30 * proportion) + 'px';
+$light.style.width = Math.round(1365 * proportion) + 'px';
